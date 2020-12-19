5201097bc66d7e1ab3ecd16058f6fc599deeaf7c
Hiroki.T <hirodora@me.com>
2020-12-19 20:37:28 +0900

    add: transparent proxy (http only)

--- detox_proxy/src/detox_proxy.nim
+++ detox_proxy/src/detox_proxy.nim
@@ -4,5 +4,34 @@
 # https://opensource.org/licenses/MIT
 
 
+import asyncdispatch
+import httpclient
+import asynchttpserver
+import uri
+
+
+proc callback(req: Request) {.async.} =
+  echo "URL"
+  echo $req.url
+
+  let client = newAsyncHttpClient()
+  let response: AsyncResponse = await client.request($req.url, req.reqMethod, req.body, req.headers)
+
+  echo "status"
+  echo response.status
+
+  var body = await response.body
+  let (isExist, bodyStreamResult) = await response.bodyStream.read
+  if isExist:
+    body = bodyStreamResult
+
+  await req.respond(response.code, body, response.headers)
+
+
+proc main =
+  let server = newAsyncHttpServer()
+  waitFor server.serve(Port(5000), callback)
+
+
 when isMainModule:
-  echo("Hello, World!")
+  main()
