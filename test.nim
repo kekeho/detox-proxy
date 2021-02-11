4dcc1f9d1175f87d1fb9a66479b0dc59b623aea3
Hiroki.T <hirodora@me.com>
2021-02-11 11:10:12 +0900

    add: http request parser

--- test.nim
+++ test.nim
@@ -1,14 +1,106 @@
 import asyncdispatch
 import asyncnet
 import net
+import tables
+import strutils
+import options
+
+
+type
+    HttpMethod = enum
+        Head,
+        Get,
+        Post,
+        Put,
+        Patch,
+        Delete,
+        Trace,
+        Options,
+        Connect,
+
+
+type
+    HttpRequest = object
+        headers*: Table[string, string]
+        reqMethod*: HttpMethod
+        body*: string
+
+
+proc toMethod(str: string): Option[HttpMethod] =
+    case str
+    of "HEAD":
+        return some(HttpMethod.Head)
+    of "GET":
+        return some(HttpMethod.Get)
+    of "POST":
+        return some(HttpMethod.Post)
+    of "PUT":
+        return some(HttpMethod.Put)
+    of "PATCH":
+        return some(HttpMethod.Patch)
+    of "DELETE":
+        return some(HttpMethod.Delete)
+    of "TRACE":
+        return some(HttpMethod.Trace)
+    of "OPTIONS":
+        return some(HttpMethod.Options)
+    of "CONNECT":
+        return some(HttpMethod.Connect)
+
+    return none(HttpMethod)
+
+
+proc httpRequestParser(raw: string): Option[HttpRequest] =
+    var req = HttpRequest()
+    let lines = raw.split("\n")
+    try:
+        # Method
+        let method_raw = lines[0]
+        let method_raw_splitted = method_raw.split(" ")
+        let method_type: Option[HttpMethod] = toMethod(method_raw_splitted[0])
+        if method_type.isNone():
+            return none(HttpRequest)
+        req.reqMethod = method_type.get()
+
+        # Header
+        var idx: int = 0
+        for idx, h in lines[1..len(lines)-1]:
+            if not (":" in h):
+                echo "break"
+                break  # end of header
+
+            let key = h.split(":")[0].strip()
+            let value = h.split(":")[1].strip()
+
+            if req.headers.hasKey(key):
+                # Join with comma https://tools.ietf.org/html/rfc7230#section-3.2
+                req.headers[key] = (req.headers[key] & ", " & value)
+            else:
+                req.headers[key] = value
+        
+        # Body
+        let body_raw = raw.split("\n")[idx+1..len(lines)-1].join("\n")
+        req.body = body_raw
+    except IndexDefect:
+        return none(HttpRequest)
+
+    return some(req)
 
 
 proc relay(from_socket: AsyncSocket, to_socket: AsyncSocket) {.async.} =
     while true:
         let data = await from_socket.recv(BufferSize)
+        let maybeReq = httpRequestParser(data)
+        if maybeReq.isNone():
+            if not from_socket.isClosed:
+                from_socket.close()
+            elif not to_socket.isClosed:
+                to_socket.close()
+            return
+        
+        let req = maybeReq.get()
+        echo req.headers
         if data.len == 0: break
-
-        echo "from->to: " & data
         await to_socket.send(data)
 
     if from_socket.isClosed:
@@ -41,4 +133,3 @@ proc serve() {.async.} =
 
 asyncCheck serve()
 runForever()
-
