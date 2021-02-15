6dde15b024c3574c64b4c64f915f8868c3ddf3e5
Hiroki.T <hirodora@me.com>
2021-02-15 16:50:04 +0900

    create: front

---
+++ front/src/Main.elm
@@ -0,0 +1,68 @@
+module Main exposing (Model, init, Msg, update, view, subscriptions)
+
+import Html exposing (..)
+import Browser
+import Browser.Navigation as Nav
+import Url
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = UrlRequested
+        , onUrlChange = UrlChanged
+    }
+
+
+type alias Model =
+    { key : Nav.Key
+    , url : Url.Url
+    }
+
+
+init : () -> Url.Url -> Nav.Key -> (Model, Cmd Msg)
+init () url key =
+    (Model key url, Cmd.none)
+
+
+type Msg
+    = UrlRequested Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        UrlRequested urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }
+            , Cmd.none
+            )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "detox-proxy"
+    , body =
+        [ div []
+            [ text "Hello, World!" ]
+      ]
+    }
+
+
