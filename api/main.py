117ec9b4e2caf5fa08c6d426855f4203cea2460b
Hiroki.T <hirodora@me.com>
2021-02-16 23:05:20 +0900

    add: POST /user create user

--- api/main.py
+++ api/main.py
@@ -1,5 +1,16 @@
-from fastapi import FastAPI
+# Copyright (c) 2021 Hiroki Takemura (kekeho)
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
 
+from fastapi import FastAPI
+from fastapi import status
+
+import schema
+import db
+
+# Init db
+db.Base.metadata.create_all(bind=db.engine)
 
 app = FastAPI(
     docs_url='/docs',
@@ -10,3 +21,18 @@ app = FastAPI(
 @app.get('/')
 async def index():
     return {'message': 'Hello, detox-proxy!'}
+
+
+@app.post(
+    '/user',
+    description='Create user',
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        status.HTTP_201_CREATED: {
+            'model': schema.User,
+            'description': 'Successful Response (created)',
+        },
+    },
+)
+def create_user(u: schema.CreateUser):
+    return u.create()
---
+++ api/schema.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2021 Hiroki Takemura (kekeho)
+
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+from pydantic import BaseModel
+from typing import List
+
+import db
+
+
+class Block(BaseModel):
+    id: int
+    url: str
+    start: int
+    end: int
+    active: bool
+
+    @classmethod
+    def from_db(cls, db_block: db.Block):
+        return cls(
+            id=db_block.id,
+            url=db_block.url,
+            start=db_block.start,
+            end=db_block.end,
+            active=db_block.active,
+        )
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+    blocklist: List[Block]
+
+    @classmethod
+    def from_db(cls, db_user: db.User):
+        return cls(
+            id=db_user.id,
+            username=db_user.username,
+            email=db_user.email,
+            blocklist=[Block.from_db(b) for b in db_user.blocklist]
+        )
+
+
+class CreateUser(BaseModel):
+    username: str
+    email: str
+    raw_password: str
+
+    def create(self) -> User:
+        with db.session_scope() as s:
+            u = db.User.create(self.username, self.email,
+                               self.raw_password)
+            s.add(u)
+            s.commit()
+
+            return User.from_db(u)
