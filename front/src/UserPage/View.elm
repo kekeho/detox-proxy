94c8c9348b8aef4a5c60181ad17dfd6f869e8737
Hiroki.T <hirodora@me.com>
2021-02-21 00:41:22 +0900

    add: block list view

--- front/src/UserPage/View.elm
+++ front/src/UserPage/View.elm
@@ -25,11 +25,29 @@ blockListPanelView user model =
 
 blockListView : User -> UserPageModel -> Html UserPageMsg
 blockListView user model =
-    div [ class "block-list" ]
-        (List.map blockRowView user.block)
+    table [ class "block-list" ]
+        [ thead []
+            [ tr [] 
+                [ th [] [ text "Active" ]
+                , th [] [ text "Host" ]
+                , th [] [ text "遮断" ]
+                , th [] [ text "再開" ]
+                ] 
+            ]
+
+        , tbody []
+            <| List.map blockRowView user.block
+        ]
 
 
 blockRowView : BlockAddress -> Html UserPageMsg
 blockRowView block =
-    div [ class "block-row" ]
-        [ text block.url ]
+    tr [ class "block-row" ]
+        [ td [ class "active" ]
+            [ input [ type_ "checkbox", checked block.active ] 
+                [] 
+            ]
+        , td [ class "host" ] [ text block.url ]
+        , td [ class "start" ] [ text <| String.fromInt block.start]
+        , td [ class "end" ] [ text <| String.fromInt block.end ]
+        ]
